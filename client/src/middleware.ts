fce0c1e08252b18ea53c2ef9025c0cb496b6283b
ngocanhuit2005 <lengocanhpyne363@gmail.com>
2025-02-11 19:40:26 +0700

    hotfix: middleware maintenance to address login problem

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -1,44 +1,37 @@
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
-import axios from "@/lib/axios";
-import { UserSessionData } from "@/utils";
-import { PAGE_ROLE } from "@/config/enums/enums";
 
 export async function middleware(req: NextRequest) {
-  console.log(req.cookies);
-  console.log(req.cookies.get("user_session"));
-  console.log(req.cookies.get("isLoggedIn"));
+  // const userSessionCookie = req.cookies.get("user_session");
+  // const isLoggedInCookie = req.cookies.get("isLoggedIn");
 
-  const userSessionCookie = req.cookies.get("user_session");
-  const isLoggedInCookie = req.cookies.get("isLoggedIn");
+  // if (
+  //   !userSessionCookie ||
+  //   !isLoggedInCookie ||
+  //   (isLoggedInCookie && isLoggedInCookie.value === "false")
+  // )
+  //   return NextResponse.redirect(
+  //     new URL("/login/?error=InformationMissing", req.url)
+  //   );
 
-  if (
-    !userSessionCookie ||
-    !isLoggedInCookie ||
-    (isLoggedInCookie && isLoggedInCookie.value === "false")
-  )
-    return NextResponse.redirect(
-      new URL("/login/?error=InformationMissing", req.url)
-    );
+  // const sessionID = userSessionCookie.value;
 
-  const sessionID = userSessionCookie.value;
+  // const encodedSessionID = encodeURIComponent(sessionID);
 
-  const encodedSessionID = encodeURIComponent(sessionID);
+  // const response = await axios.get(
+  //   `/auth/session/?sessionId=${encodedSessionID}`
+  // );
 
-  const response = await axios.get(
-    `/auth/session/?sessionId=${encodedSessionID}`
-  );
+  // const userSessionData: UserSessionData = response.data.data;
 
-  const userSessionData: UserSessionData = response.data.data;
+  // const currentUrl = req.nextUrl.pathname;
 
-  const currentUrl = req.nextUrl.pathname;
+  // const isHasRequiredRoles = userSessionData.roles.some(
+  //   (role) => role === PAGE_ROLE.ADMIN || role === PAGE_ROLE.MANAGER
+  // );
 
-  const isHasRequiredRoles = userSessionData.roles.some(
-    (role) => role === PAGE_ROLE.ADMIN || role === PAGE_ROLE.MANAGER
-  );
-
-  if (currentUrl.startsWith("/home/admin") && !isHasRequiredRoles)
-    return NextResponse.redirect(new URL("/unauthorized", req.url));
+  // if (currentUrl.startsWith("/home/admin") && !isHasRequiredRoles)
+  //   return NextResponse.redirect(new URL("/unauthorized", req.url));
 
   return NextResponse.next();
 }
